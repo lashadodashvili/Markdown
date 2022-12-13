084ce1a1864205eecd896afae172aa8f43713c74
lashadodashvili <107387918+lashadodashvili@users.noreply.github.com>
2022-12-13 17:59:05 +0400

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import ReactMarkdown from 'react-markdown'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [markdown, setMarkDown] = useState('## markdown preview')
+
+
+
+  return (<main>
+    <seciton className="markdown" >
+      <textarea className='input'
+        value={markdown}
+        onChange={(e) => setMarkDown(e.target.value)} >
+
+      </textarea>
+      <article className='result'>
+        <ReactMarkdown>
+          {markdown}
+        </ReactMarkdown>
+      </article>
+    </seciton>
+
+  </main>
+  )
 }
 
-export default App;
+export default App
